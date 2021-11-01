539880b24c043fc20f149f57c7d65db1331750cb
bruni6072 <72555487+bruni6072@users.noreply.github.com>
2021-11-01 14:08:32 +0000

    Updated HelloConsole.cs with my name

--- ThemePark1.0-Copy/helloConsole.cs
+++ ThemePark1.0-Copy/helloConsole.cs
@@ -1,2 +1,21 @@
 //helloConsole.cs example
 //Cristinas Example!  --->   test 1
+
+sing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Cristinas Example");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
